a02b5f4133fc2ae585197e48d03ff73ee5fbc05a
Bryan Cruz <bscruz@ucsd.edu>
2025-10-24 20:57:31 -0700

    Create projects.js

---
+++ projects/projects.js
@@ -0,0 +1,19 @@
+import { fetchJSON, renderProjects } from '../global.js';
+
+(async function () {
+  const projects = await fetchJSON('../lib/projects.json');
+
+  if (!projects) {
+    console.error("No projects loaded.");
+    return;
+  }
+
+  const projectsContainer = document.querySelector('.projects');
+
+  if (!projectsContainer) {
+    console.error("No .projects container found in the HTML.");
+    return;
+  }
+
+  renderProjects(projects, projectsContainer, 'h2');
+})();
