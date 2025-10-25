b3c03506bab947e57e77905aa0b8bdfd43bac8be
Bryan Cruz <bscruz@ucsd.edu>
2025-10-24 22:10:15 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+import { fetchJSON, renderProjects } from './global.js';
+
+(async function () {
+  const projects = await fetchJSON('./lib/projects.json');
+
+  if (!projects) {
+    console.error("No projects loaded.");
+    return;
+  }
+
+  const latestProjects = projects.slice(0, 3);
+
+  const projectsContainer = document.querySelector('.projects');
+
+  if (!projectsContainer) {
+    console.error("No .projects container found in the HTML.");
+    return;
+  }
+  
+  renderProjects(latestProjects, projectsContainer, 'h2');
+})();
